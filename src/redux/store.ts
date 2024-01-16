1acdfc3686818224c083322d055838c1bd481caf
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2024-01-16 08:35:58 +0100

    ++

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,17 @@
 import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
 import { api } from "./services/api";
 import auth from "./slices/authSlice";
+import { listenerMiddleware } from "../middleware/authMid";
 
 const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(api.middleware)
+      .prepend(listenerMiddleware.middleware),
 });
 
 export default store;
